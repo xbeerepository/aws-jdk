3cbcd5d9538342e4ee06f37d892d7bf14d7bf8fe
Eric Fischer <efischer78@yahoo.fr>
2023-04-09 20:21:53 +0200

    ok

--- templates/link.sh
+++ templates/link.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-a=$(find {{ .pack.installDir }} -maxdepth 1 -mindepth 1 -type d -name "amazon*" -printf '%f\n')
-ln -s {{ .pack.installDir }}/$a {{ .pack.javaHome }}
+a=$(find {{ .installDir }} -maxdepth 1 -mindepth 1 -type d -name "amazon*" -printf '%f\n')
+ln -s {{ .installDir }}/$a {{ .javaHome }}
