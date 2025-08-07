3c3726ed93d66ccdb5a298050849de37a62465fc
Eric Fischer <efischer78@yahoo.fr>
2025-08-07 14:50:18 +0200

    ok

--- templates/link.sh
+++ templates/link.sh
@@ -3,3 +3,4 @@ set -e
 
 a=$(find {{ .xbee.install }} -maxdepth 1 -mindepth 1 -type d -name "amazon*" -printf '%f\n')
 ln -s {{ .xbee.install }}/$a {{ .javaHome }}
+chown -R {{ .xbee.user}}:{{ .xbee.group }} {{ .xbee.install }}/$a
